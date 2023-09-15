e0990ba1438b174475646ff4fb4e064b1612836a
Ariel <>
2023-09-15 16:01:00 -0400

    added some comments to the eslint config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  // All eslint rules. Prettier last to overwrite eslint rules
   extends: [
     "airbnb-base",
     "airbnb-typescript/base",
@@ -9,8 +10,10 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
+  // path to the typescript config file
   parserOptions: {
     project: "./tsconfig.json",
   },
+  // Exclude this file
   ignorePatterns: [".eslintrc.cjs"],
 };
